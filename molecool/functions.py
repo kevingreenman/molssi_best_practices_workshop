cd32c8ab2d77115e397feeefad2bb64b0a38b35e
Kevin Greenman <kgreenman77@gmail.com>
2021-01-13 09:55:59 -0500

    catch up on missed stuff, add functions and tests from hw

--- molecool/functions.py
+++ molecool/functions.py
@@ -23,7 +23,7 @@ def calculate_distance(rA, rB):
     Returns
     -------
     dist : float
-        The distance between two points
+        The distance between the two points.
 
     Examples
     --------
@@ -32,6 +32,7 @@ def calculate_distance(rA, rB):
     >>> calculate_distance(r1, r2)
     0.1
     """
+
     d = rA - rB
     dist = np.linalg.norm(d)
     return dist
@@ -175,8 +176,37 @@ def bond_histogram(bond_list, save_location=None, dpi=300, graph_min=0, graph_ma
 
 
 def build_bond_list(coordinates, max_bond=1.5, min_bond=0):
+    """
+    Calculate bonds in a molecule base on a distance criteria.
 
-    # Find the bonds in a molecule (set of coordinates) based on distance criteria.
+    The pairwise distance between atoms is computed. If it is in the range 
+    `min_bond` to `max_bond`, the atoms are counted as bonded.
+
+    Parameters
+    ----------
+    coordinates : array-like
+        The coordinates of the atoms.
+    max_bond : float (optional)
+        The maximum distance for two points to be considered bonded. The default
+        is 1.5
+    min_bond : float (optional)
+        The minimum distance for two points to be considered bonded. The default
+        is 0.
+    
+    Returns
+    -------
+    bonds : dict
+        A dictionary where the keys are tuples of the bonded atom indices, and the
+        associated values are the bond length.
+    """
+
+    if min_bond < 0:
+        raise ValueError("Bond length can not be less than zero.")
+
+    if len(coordinates) < 1:
+        raise ValueError("Bond list can not be calculated for coordinate length less than 1.")
+    
+    # Find the bonds in a molecule
     bonds = {}
     num_atoms = len(coordinates)
 
@@ -187,6 +217,61 @@ def build_bond_list(coordinates, max_bond=1.5, min_bond=0):
                 bonds[(atom1, atom2)] = distance
 
     return bonds
+
+def calculate_molecular_mass(symbols):
+    """Calculate the mass of a molecule.
+    
+    Parameters
+    ----------
+    symbols : list
+        A list of elements.
+    
+    Returns
+    -------
+    mass : float
+        The mass of the molecule
+    """
+
+    mass = 0
+    for atom in symbols:
+        mass += atomic_weights[atom]
+    
+    return mass
+
+def calculate_center_of_mass(symbols, coordinates):
+    """Calculate the center of mass of a molecule.
+    
+    The center of mass is weighted by each atom's weight.
+    
+    Parameters
+    ----------
+    symbols : list
+        A list of elements for the molecule
+    coordinates : np.ndarray
+        The coordinates of the molecule.
+    
+    Returns
+    -------
+    center_of_mass: np.ndarray
+        The center of mass of the molecule.
+    Notes
+    -----
+    The center of mass is calculated with the formula
+    
+    .. math:: \\vec{R}=\\frac{1}{M} \\sum_{i=1}^{n} m_{i}\\vec{r_{}i}
+    
+    """
+
+    total_mass = calculate_molecular_mass(symbols)
+    
+    mass_array = np.zeros([len(symbols), 1])
+    
+    for i in range(len(symbols)):
+        mass_array[i] = atomic_weights[symbols[i]]
+    
+    center_of_mass = sum(coordinates * mass_array) / total_mass
+    
+    return center_of_mass
 
 
 atom_colors = {
@@ -200,3 +285,31 @@ atom_colors = {
     "Br": "#F4A460",
     "S": "yellow",
 }
+
+
+def canvas(with_attribution=True):
+    """
+    Placeholder function to show example docstring (NumPy format)
+
+    Replace this function and doc string for your own project
+
+    Parameters
+    ----------
+    with_attribution : bool, Optional, default: True
+        Set whether or not to display who the quote is from
+
+    Returns
+    -------
+    quote : str
+        Compiled string including quote and optional attribution
+    """
+
+    quote = "The code is but a canvas to our imagination."
+    if with_attribution:
+        quote += "\n\t- Adapted from Henry David Thoreau"
+    return quote
+
+
+if __name__ == "__main__":
+    # Do something if this file is invoked on its own
+    print(canvas())
